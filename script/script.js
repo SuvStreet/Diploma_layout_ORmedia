445dccd06e6469c2374e9af5a0eeea5f5b2c30ec
Дмитрий Колотуша <suvstreet@gmail.com>
2020-05-11 01:02:10 +0300

    update responsive add burger add scss sass

--- script/script.js
+++ script/script.js
@@ -1,10 +1,24 @@
-window.onload = function () {
-    document.querySelector(".carousel").onclick() = function () {
-        document
-            .querySelector(".active")
-            .nextElementSibling.classList.add("minus");
-        document
-            .querySelector(".active")
-            .previousElementSibling.classList.add("minus");
-    };
+function editSlide(){
+    document.querySelector('.owl-item.active').querySelector('.img img').classList.remove('shows');
+    if(document.querySelector('.owl-item.active').previousElementSibling){
+        document.querySelector('.owl-item.active').previousElementSibling.querySelector('.img img').classList.add('shows');
+    }
+    if(document.querySelector('.owl-item.active').nextElementSibling){
+        document.querySelector('.owl-item.active').nextElementSibling.querySelector('.img img').classList.add('shows');
+    }
+}
+
+window.onload = function(){
+    editSlide();
+}
+document.querySelector('.container5').addEventListener('touchmove', editSlide);
+document.querySelector('.container5').addEventListener('mousemove', editSlide);
+
+
+document.getElementById('burger').onclick = function(){
+    addMenu();
 };
+
+function addMenu(){
+    document.getElementById('menu_background').classList.toggle('show')
+}
